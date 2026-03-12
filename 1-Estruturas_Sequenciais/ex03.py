ef95fde3151409fedaef95950cfb9555312990af
Herald Fortunato <109013609+Fortunato-Herald@users.noreply.github.com>
2026-03-11 21:53:50 -0300

    Adicionado o exercicio 3

---
+++ 1-Estruturas_Sequenciais/ex03.py
@@ -0,0 +1,8 @@
+"""
+Faça um programa que peça dois números e imprima a soma:
+"""
+
+numero1 = input("Digite o primeiro número: ")
+numero2 = input("Digite o segundo número: ")
+soma = float(numero1) + float(numero2)
+print(f"A soma dos números é: {soma}")
